4f52cc1d26ec4621a647473dd051087b840a6ecb
arkapravabanerjee <arkaprava.banerjee@gmail.com>
2019-05-28 19:49:01 +0530

    Updates

---
+++ precache-manifest.dded3af1091d463537e2b190f82b563e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d01abf2f36e48a7db045ef7f0bbb789",
+    "url": "/quickshop/index.html"
+  },
+  {
+    "revision": "2d4d04198351ceb62828",
+    "url": "/quickshop/static/css/main.dc44857f.chunk.css"
+  },
+  {
+    "revision": "c3314009a8b38bea0db5",
+    "url": "/quickshop/static/js/2.77f90bf7.chunk.js"
+  },
+  {
+    "revision": "2d4d04198351ceb62828",
+    "url": "/quickshop/static/js/main.0f261d14.chunk.js"
+  },
+  {
+    "revision": "7160719d2e9051ddccd8",
+    "url": "/quickshop/static/js/runtime~main.57aa9673.js"
+  }
+]);
